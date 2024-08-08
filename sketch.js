a338faf4fe7e3ed88e41d5e083d63c759fe57cd8
Pedro Augusto S. Schmoller <Pedro.Schmoller@escola.pr.gov.br>
2024-08-08 16:45:37 -0300

    adicionar arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(650,600);
+  background("white");
+  }
+  
+  function draw() {
+  rect(30,20,20)
+  fill("purple");
+  stroke("red");
+  if(mouseIsPressed);{
+  rect(mouseX,mouseY,20,20);}}
+  
+  
